712a6109bbc56390fbedcf30182564ec3c7ab39a
zoomjuice <itsjunkmaill@gmail.com>
2021-03-10 07:56:57 -0500

    Add logic to find traveler destinations and convert to internal data

--- Unit_04-BoredlessTourist/04_01_Project-BoredlessTourist.py
+++ Unit_04-BoredlessTourist/04_01_Project-BoredlessTourist.py
@@ -1,3 +1,20 @@
 destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+
+def get_destination_index(destination):
+    dest_index = destinations.index(destination)
+    return dest_index
+
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    trav_dest_index = get_destination_index(traveler_destination)
+    return trav_dest_index
+
+
+test_dest_index = get_traveler_location(test_traveler)
+
+
+print(test_dest_index)
