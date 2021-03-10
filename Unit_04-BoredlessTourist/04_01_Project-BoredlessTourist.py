bac63e827a6465d8e60669c9731ef6f8c8301659
zoomjuice <itsjunkmaill@gmail.com>
2021-03-10 07:44:17 -0500

    Add test objects

--- Unit_04-BoredlessTourist/04_01_Project-BoredlessTourist.py
+++ Unit_04-BoredlessTourist/04_01_Project-BoredlessTourist.py
@@ -1,1 +1,3 @@
-"foo" 
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
