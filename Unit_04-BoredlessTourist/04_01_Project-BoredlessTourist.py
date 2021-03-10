c55e74ef1493c61b9761f08b315c7bc19c5bdd85
zoomjuice <itsjunkmaill@gmail.com>
2021-03-10 08:06:31 -0500

    Create attractions and functionality for adding new attractions

--- Unit_04-BoredlessTourist/04_01_Project-BoredlessTourist.py
+++ Unit_04-BoredlessTourist/04_01_Project-BoredlessTourist.py
@@ -1,4 +1,5 @@
 destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+attractions = [[] for dest in destinations]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
@@ -14,7 +15,25 @@ def get_traveler_location(traveler):
     return trav_dest_index
 
 
-test_dest_index = get_traveler_location(test_traveler)
+def add_attraction(destination, attraction):
+    try:
+        dest_index = get_destination_index(destination)
+    except ValueError:
+        return
+    attractions[dest_index].append(attraction)
+    return
 
 
-print(test_dest_index)
+add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
